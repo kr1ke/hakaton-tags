7638d6736e1c0aa76dcb8ea83e864ef7486360c3
imfreedomyt <imfreedomyt@gmail.com>
2022-04-19 17:24:34 +0300

    gg

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 import router from "../router";
 export var api = axios.create({
-  headers: {
-    "Content-type": "application/json",
-  },
-  baseURL: 'http://localhost:5000',
-  responseType: "json",
+    headers: {
+        "Content-type": "application/json",
+    },
+    baseURL: 'https://6671-95-179-127-177.ngrok.io',
+    responseType: "json",
 });
 
 // проверка авториации при каждом запросе
 api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response.status === 401) {
-      // AuthServices.logout();
-      router.push('/auth')
-      localStorage.clear()
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            // AuthServices.logout();
+            router.push('/auth')
+            localStorage.clear()
 
+        }
+        return error;
     }
-    return error;
-  }
 );
